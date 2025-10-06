2d53a7086c6487e362de5fb82470755e40e9cadf
Ge0m <35548187+Ge0m@users.noreply.github.com>
2025-10-06 03:38:52 +0000

    fixed naming

--- apps/analyzer/vite.config.js
+++ apps/analyzer/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/react-app-test/analyzer/',
+  base: '/SZMatchBuilder/analyzer/',
   build: {
     outDir: '../../docs/analyzer',
     emptyOutDir: true
