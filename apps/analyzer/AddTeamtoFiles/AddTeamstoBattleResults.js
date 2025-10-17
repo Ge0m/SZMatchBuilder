d4ca9cf5322e75bc2ac8da18802ca1048224ab80
Ge0m <geldi.omeri@gmail.com>
2025-10-16 21:05:07 -0500

    Added Tosh data

---
+++ apps/analyzer/AddTeamtoFiles/AddTeamstoBattleResults.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderPath = 'D:/DBZL/SZLeague/GitHub/SZMatchBuilder/BattleResultFiles';
+const teamsToAdd = ["Tiny Terros", "Malevolant Souls"];
+
+fs.readdirSync(folderPath).forEach(file => {
+  if (file.endsWith('.json')) {
+    const filePath = path.join(folderPath, file);
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+    // Set TeamBattleResults.teams
+    if (data.TeamBattleResults) {
+      data.TeamBattleResults.teams = [...teamsToAdd];
+    } else {
+      console.warn(`No TeamBattleResults found in ${file}`);
+    }
+
+    // Set top-level teams
+    data.teams = [...teamsToAdd];
+
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf8');
+    console.log(`Updated: ${file}`);
+  }
+});
